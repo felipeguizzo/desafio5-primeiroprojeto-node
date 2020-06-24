2f0f10709e5466396f9394406589eba201d7018f
felipeguizzo <guuizzo@hotmail.com>
2020-06-24 22:43:19 +0100

    finished

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,11 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactioDTO {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -14,15 +20,36 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const allTransactions = this.all();
+
+    const sumIncome = allTransactions
+      .filter(x => x.type === 'income')
+      .map(x => x.value)
+      .reduce((acc, val) => acc + val, 0);
+
+    const filterOutcome = allTransactions.filter(x => x.type === 'outcome');
+
+    let sumOutcome = 0;
+    if (filterOutcome.length > 0) {
+      // eslint-disable-next-line prettier/prettier
+      sumOutcome = filterOutcome.map(x => x.value)
+        .reduce((acc, val) => acc + val, 0);
+    }
+    const sumTotal = sumIncome - sumOutcome;
+
+    return { income: sumIncome, outcome: sumOutcome, total: sumTotal };
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactioDTO): Transaction {
+    const transaction = new Transaction({ title, value, type });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
